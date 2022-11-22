c8639a0d70a09080858c3810aca42bd40bc1abb7
ALYSSA_MAPLES <armaples@ucsc.edu>
2022-11-21 20:02:28 -0500

    Autocomplete function nearly functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,219 +1,48 @@
 // Create array of all Marvel Characters
 var marvelCharacters = [];
-var requestUrl0 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=0';
-var requestUrl1 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=100';
-var requestUrl2 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=200';
-var requestUrl3 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=300';
-var requestUrl4 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=400';
-var requestUrl5 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=500';
-var requestUrl6 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=600';
-var requestUrl7 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=700';
-var requestUrl8 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=800';
-var requestUrl9 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=900';
-var requestUrl10 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=1000';
-var requestUrl11 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=1100';
-var requestUrl12 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=1200';
-var requestUrl13 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=1300';
-var requestUrl14 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=1400';
-var requestUrl15 = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56&limit=100&offset=1500';
 
 // Gets names 
 $(document).ready(function() {
-    fetch(requestUrl0)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
 
-    fetch(requestUrl1)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
+    var requestUrl = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f28e122cf392b150e1e8bf5ca2a62842&hash=3abdba83f20f29cc7e578a0e44005e56";
+        console.log(requestUrl);
 
-    fetch(requestUrl2)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl3)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl4)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl5)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl6)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl7)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl8)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl9)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl10)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl11)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl12)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl13)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl14)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl15)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
-
-    fetch(requestUrl16)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        };
-    })
+    // Create autocomplete function for input field
+    $("#userInput").autocomplete({
+        minLength: 1,
+        source: function(request, response, url){
+            $.ajax({
+                url: requestUrl,
+                dataType: "json",
+                data: {
+                    nameStartsWith: request.term,
+                    limit: 10,
+                },
+                success: function (data) {
+                    console.log(data)
+                    response($.map(data.data.results, function(item) {
+                        for (let i = 0; i < 10; i++) {
+                            console.log(data.data.results[i].name)
+                            return data.data.results[i].name
+                        }
+                    }))
+                }
+            })
+        }
+    });
 });
+        
+        
+            
+                
+                
+                    
+                    
+                            
+                            
 
-// Create autocomplete function for input field
-function autoComplete() {
-    // define variable
-    // add event listener for input
-    // create div element to contain items
-    // for loop 
-}
+
+
     const options = {
         method: 'GET',
         headers: {
@@ -227,8 +56,4 @@ function autoComplete() {
     fetch(apiUrl, options)
         .then(response => response.json())
         .then(response => console.log(response))
-<<<<<<< HEAD
-        .catch(err => console.error(err));
-=======
-        .catch(err => console.error(err));
->>>>>>> main
+        .catch(err => console.error(err))
